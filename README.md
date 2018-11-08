3b930d5eec02bcfbbe794f02aa58484b37a6f6c8
Zhi Li <zli@babson.edu>
2018-11-08 12:51:04 -0500

    added all files

---
+++ README.md
@@ -0,0 +1,10 @@
+# Exam 2017Fall
+
+## Exam Instruction
+
+* The exam will be available starting from Tuesday 11/14 at 1:15 pm.
+* The exam will close on Wednesday 11/15 at 1:15 pm.
+* You need to submit two .py files via Blackboard - Session 21.
+* You can use all class materials. You can use the internet to look up documentation and to clarify your knowledge of certain topics.
+* You can NOT request or obtain help from others in any format, including but not limited to posting an ask-for-help message in any website, talking to others for help, emailing exam to others. Any one who looks for help from others would get zero for the exam.
+* Check Slack periodically in case Professor Li might make some clarification on questions.
---
+++ exam_p1.py
@@ -0,0 +1,31 @@
+trans = {'0':'ling', '1':'yi', '2':'er', '3':'san', '4': 'si', '5':'wu', 
+         '6':'liu', '7':'qi', '8':'ba', '9':'jiu', '10': 'shi', '100': 'bai'}
+
+
+
+def speak_Chinese(number):
+    '''
+    number: an integer, 0<=number<=999
+
+    Returns: a string that is the number in Chinese
+    '''
+    pass
+
+
+# For testing
+def main():
+    print(speak_Chinese(36))
+    print('In Chinese: 36 = san shi liu')
+    print(speak_Chinese(20))
+    print('In Chinese: 20 = er shi')
+    print(speak_Chinese(16))
+    print('In Chinese: 16 = shi liu')
+    print(speak_Chinese(200))
+    print('In Chinese: 200 = er bai')
+    print(speak_Chinese(109))
+    print('In Chinese: 109 = yi bai ling jiu')
+    print(speak_Chinese(999))
+    print('In Chinese: 999 = jiu bai jiu shi jiu')
+
+if __name__ == '__main__':
+    main()
